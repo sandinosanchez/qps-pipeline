81779492195803763e8e1cf9fa105acef2017855
tsvrko <tsvrko@gmail.com>
2019-06-06 14:38:40 +0300

    build result comparison is changed

--- src/com/qaprosoft/jenkins/pipeline/runner/maven/QARunner.groovy
+++ src/com/qaprosoft/jenkins/pipeline/runner/maven/QARunner.groovy
@@ -479,12 +479,9 @@ public class QARunner extends AbstractRunner {
                     testRun = zafiraUpdater.getTestRunByCiRunId(uuid)
                     if (!isParamEmpty(testRun)) {
                         def abortedTestRun = zafiraUpdater.abortTestRun(uuid, currentBuild)
-                        logger.info("1111111")
-                        logger.info(abortedTestRun.dump())
-                        logger.info(currentBuild.rawBuild.dump())
-                        if (!isParamEmpty(abortedTestRun)
+                        if ((!isParamEmpty(abortedTestRun)
                                 && !StatusMapper.ZafiraStatus.ABORTED.name().equals(abortedTestRun.status)
-                                && !BuildResult.ABORTED.name().equals(currentBuild.rawBuild.result) || Configuration.get("notify_slack_on_abort")?.toBoolean()) {
+                                && BuildResult.ABORTED != currentBuild.rawBuild.result) || Configuration.get("notify_slack_on_abort")?.toBoolean()) {
                             zafiraUpdater.sendSlackNotification(uuid, Configuration.get("slack_channels"))
                         }
                     }
